901ccf1a3ace6020135bddfe8a57978b59d36e4b
Mark Bran <47259119+markytobran@users.noreply.github.com>
2022-08-20 12:59:24 +0100

    feat: Adds api key validation to the create video route

--- server/src/modules/video/video.route.ts
+++ server/src/modules/video/video.route.ts
@@ -1,15 +1,14 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
+import validateAPIkey from '../../middleware/validateAPIkey'
 import { createVideoHandler, getVideosHandler, getTOPVideosHandler, getVideoHandler, getVideosByChannelIdHandler } from './video.controller'
 import { createVideoSchema, getVideoParamsSchema, getChannelIDParamsSchema } from './video.schema'
 
 export function videosRoute(app: FastifyInstance, options: FastifyPluginOptions, done: () => void) {
   app.get('/', getVideosHandler)
-  app.post('/', { schema: createVideoSchema }, createVideoHandler) //check api key
+  app.post('/', { preHandler: validateAPIkey, schema: createVideoSchema }, createVideoHandler)
   app.get('/:id', { schema: getVideoParamsSchema }, getVideoHandler)
   app.get('/topvideos/home', getTOPVideosHandler)
   app.get('/channel/:id', { schema: getChannelIDParamsSchema }, getVideosByChannelIdHandler)
-  // router.get('/api/videos/channel/:id', validateResource(getVideoSchema), getVideosByChannelIdHandler)
-  // router.get('/api/videos/category/:categoryKey/:value', getVideosByCategoryHandler)
 
   done()
 }
